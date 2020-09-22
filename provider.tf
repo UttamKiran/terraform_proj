431661d4f37206436f81922630d8a7aaca8fa660
UttamKiran <ukchavan.chavan@gmail.com>
2020-09-22 11:01:24 -0400

    terraform dev branch  file commit

--- provider.tf
+++ provider.tf
@@ -1,13 +1,14 @@
 provider "aws"{
-  region = "ap-south-1"
+   region = "ap-south-1"
 }
+
 
 resource "aws_vpc" "my_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "JavaHome"
+    Name = "DisneyHome"
     Environment = "Dev"
   }
 }
