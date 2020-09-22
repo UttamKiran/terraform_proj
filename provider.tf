d66b7a5970ef5af0b8658401ee2b40fd050e12c6
UttamKiran <ukchavan.chavan@gmail.com>
2020-09-22 10:57:56 -0400

    terraform file commit

---
+++ provider.tf
@@ -0,0 +1,24 @@
+provider "aws"{
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "JavaHome"
+    Environment = "Dev"
+  }
+}
+
+output "vpc_cidr" {
+
+   value="${aws_vpc.my_vpc.cidr_block}"
+}
+
+output "vpc_owner" {
+
+   value="${aws_vpc.my_vpc.owner_id}"
+
+}
